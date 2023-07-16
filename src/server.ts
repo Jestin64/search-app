e5a4bce83911347991a5fe7357bd05a3b0893d17
jestin <jestin.jois@xtraininglive.com>
2023-07-16 10:54:23 +0530

    added mongoose conc

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,28 @@
 import express from "express";
 import redis from "redis";
 import mongoose from "mongoose";
+import "dotenv/config";
 
 // const Document = require('./models/document');
 // const redisClient = redis.createClient();
 
 const app = express();
-
 app.use(express.json());
 
-app.listen(3000, ()=>{
-    console.log("server started at port 3000")
+// Connect to MongoDB and start the server
+mongoose.connect(`mongodb+srv://AaronBaron:${process.env.MONGO_PASS}@cluster0.syfka.gcp.mongodb.net/?retryWrites=true&w=majority`,{
+    dbName: "search-app",
 })
+    .then(() => app.listen(3000, () => console.log('Server running at port 3000')))
+    .catch((err) => {
+        throw new Error(err);
+    } );
+
+
+//todo:  once mongoose is up then deprecate this
+// app.listen(3000, ()=>{
+//     console.log("server started at port 3000")
+// })
 
 // app.post('/document', async (req, res) => {
 //     const { author, date, name, text } = req.body;
@@ -54,8 +65,4 @@ app.listen(3000, ()=>{
 //     });
 // });
 
-// Connect to MongoDB and start the server
 
-// mongoose.connect('mongodb://localhost:27017/searchApp', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => app.listen(5000, () => console.log('Server running')))
-//     .catch((err) => console.log(err));
